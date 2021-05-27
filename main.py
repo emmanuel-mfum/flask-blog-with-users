362709ef544ca39a7fd35991e84f187a5010b012
Emmanuel <emmanuel.mfumuangani@protonmail.com>
2021-05-27 18:01:56 -0400

    Configure env variables and add postgre connection

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -46,7 +46,7 @@ def load_user(user_id):  # function for reloading the user from its user id
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
